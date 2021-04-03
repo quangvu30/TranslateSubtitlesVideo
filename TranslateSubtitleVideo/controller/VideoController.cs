7f50e86d81a21751305e756454ab9eeed46254d5
quangvd30 <docnhatvonhi40@gmail.com>
2021-04-03 15:10:24 +0700

    main

---
+++ TranslateSubtitleVideo/controller/VideoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranslateSubtitleVideo.functions;
+
+namespace TranslateSubtitleVideo.controller
+{
+    class VideoController
+    {
+        string folderFrames;
+        public async Task GetSubtitlesFromVideo()
+        {
+            string[] Images = Directory.GetFiles(folderFrames);
+
+            foreach (string image in Images)
+            {
+                string fileName = Path.GetFileName(image);
+                Bitmap b = (Bitmap)Bitmap.FromFile(image);
+                DetectText detectText = new DetectText(env.Default.KeyId, env.Default.SecretKey);
+                List<string> contents = await detectText.TextDetection(b);
+                CreateSubtitles.CreateFileSubtitle(int.Parse(fileName), contents);
+            }
+        }
+
+        void SplitFrames()
+        {
+
+        }
+    }
+}
---
+++ TranslateSubtitleVideo/view/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TranslateSubtitleVideo.controller;
+using TranslateSubtitleVideo.models;
+
+namespace TranslateSubtitleVideo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FirefoxController firefox = new FirefoxController();
+            //firefox.Translate(@"D:\Project\BTLLLL\VideoProcessing\VideoProcessing\bin\Debug\EngSub.txt");
+            CreateSubtitles.CreateFileSubtitle(306, new List<string>() { "Quang" });
+            Console.WriteLine(env.Default.KeyId + ", " + env.Default.SecretKey);
+        }
+    }
+}
